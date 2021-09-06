c2af1d6faf88a1d30fd1c7fd418f6dc5aaf90ce7
Kevin Luo <kwluo90@gmail.com>
2021-09-06 13:27:10 -0700

    Populate todo list from remote API

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,40 @@
 import Head from 'next/head'
-import { Heading, Input, Button, Checkbox, Text, Code, Flex, Box } from "@chakra-ui/react";
+import { Heading, Input, Button, Checkbox, Text, VStack, Code, Flex, Box } from "@chakra-ui/react";
+import useSWR, { SWRConfig } from 'swr'
 
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  const dummyTodoItems = [
-    {
-      id: 1,
-      todo: 'foo',
-    },
-    {
-      id: 2,
-      todo: 'bar',
-    },
-    {
-      id: 3,
-      todo: 'baz',
-    },
-  ];
+const API = 'http://localhost:3001/todos';
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
+export async function getServerSideProps(context) {
+  const todosResult = await fetcher(API);
+
+  return {
+    props: {
+      fallback: {
+        [API]: todosResult
+      }
+    }
+  }
+}
+
+function TodoList() {
+  const { data } = useSWR(API, fetcher);
+
+  console.log('Is data ready?', !!data);
+
+  if (!data) return <Text>Loading...</Text>
+  return (
+    <div>
+      <VStack spacing={1} align="left">
+        {data.map(item => <Checkbox key={item.id} defaultChecked={item.completed}>{item.text}</Checkbox>)}
+      </VStack>
+    </div>
+  )
+}
+
+export default function Home({ fallback }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -36,7 +52,9 @@ export default function Home() {
           <Button colorScheme="blue">Add</Button>
         </Flex>
 
-        {dummyTodoItems.map(item => <Checkbox>{item.todo}</Checkbox>)}
+        <SWRConfig value={{ fallback }}>
+          <TodoList />
+        </SWRConfig>
       </main>
 
       <footer className={styles.footer}>
